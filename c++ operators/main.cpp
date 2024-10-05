80e16aaec0de6794a66661af80cf61e090f0daf3
Vikash Kumar <vk4845646@gmail.com>
2024-10-05 16:46:15 +0530

    Add files via upload

---
+++ c++ operators/main.cpp
@@ -0,0 +1,13 @@
+// Losical Operators
+#include <iostream>
+using namespace std;
+int main(){
+bool a=true;
+bool b=false;
+
+cout<<(a&&b)<<endl;
+cout<<(a||b)<<endl;
+cout<<(!a)<<endl;
+
+ return 0;
+}
